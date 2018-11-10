c8fc1b257f97284536ae6a3ff44f7869373d637d
Gebb <gebb.freelancer@gmail.com>
2018-11-10 09:47:39 +0800

    v1-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularAmazono';
+  searchTerm='';
+  isCollapsed=true;
+ 
+
+  getToken(){
+    return localStorage.getItem('token');
+
+  }
+
+  collapse(){
+    this.isCollapsed=true;
+  }
+
+  closeDropdown(dropdown){
+    dropdown.close();
+
+  }
+
+  logout(){}
+
+  search(){}
 }
